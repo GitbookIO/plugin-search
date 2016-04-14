3e6bff962264b9e50c173177a9b5d50f0375c38c
Samy Pessé <samypesse@gmail.com>
2016-04-14 12:58:05 +0200

    Remove lunr

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# plugin-search
+# search
 
-This plugin adds interactive search in sidebar
+This plugin is a default plugin for GitBook, it adds an interactive search bar to your book.
+
+This plugin is backend agnostic.
 
 ### Disable this plugin
 
@@ -11,4 +13,11 @@ This is a default plugin and it can be disabled using a `book.json` configuratio
     plugins: ["-search"]
 }
 ```
+
+### Backends
+
+| Backend | Plugin Name | Description |
+| ------- | ----------- | ----------- |
+| [Lunr](https://github.com/GitbookIO/plugin-lunr) | `lunr` | Index the content into a local/offlien index |
+| [Algolia](https://github.com/GitbookIO/plugin-algolia) | `algolia` | Index the content in Algolia |
 
--- index.js
+++ index.js
@@ -1,74 +1,12 @@
-var lunr = require('lunr');
-
-// Create search index
-var searchIndex = lunr(function () {
-    this.ref('url');
-
-    this.field('title', { boost: 10 });
-    this.field('body');
-});
-
-// Map of Lunr ref to document
-var documentsStore = {};
-
-var searchIndexEnabled = true;
-var indexSize = 0;
-
 module.exports = {
     book: {
         assets: './assets',
         js: [
-            'lunr.min.js', 'search-engine.js', 'search-lunr.js', 'search.js'
+            'search-engine.js', 'search.js'
         ],
         css: [
             'search.css'
         ]
-    },
-
-    hooks: {
-        // Index each page
-        'page': function(page) {
-            if (this.output.name != 'website' || !searchIndexEnabled) return page;
-
-            var text, maxIndexSize;
-            maxIndexSize = this.config.get('pluginsConfig.search.maxIndexSize') || this.config.get('search.maxIndexSize');
-
-            this.log.debug.ln('index page', page.path);
-
-            // Transform as TEXT
-            text = page.content.replace(/(<([^>]+)>)/ig, '');
-
-            indexSize = indexSize + text.length;
-            if (indexSize > maxIndexSize) {
-                this.log.warn.ln('search index is too big, indexing is now disabled');
-                searchIndexEnabled = false;
-                return page;
-            }
-
-            // Add to index
-            var doc = {
-                url: this.output.toURL(page.path),
-                title: page.title,
-                summary: page.description,
-                body: text
-            };
-
-            documentsStore[doc.url] = doc;
-            searchIndex.add(doc);
-
-            return page;
-        },
-
-        // Write index to disk
-        'finish': function() {
-            if (this.output.name != 'website') return;
-
-            this.log.debug.ln('write search index');
-            return this.output.writeFile('search_index.json', JSON.stringify({
-                index: searchIndex,
-                store: documentsStore
-            }));
-        }
     }
 };
 
